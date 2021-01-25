de622cb2614ae39c1f238c65f58c3b221494c110
emanuelerre <67638573+emanuelerre@users.noreply.github.com>
2021-01-25 16:10:37 +0100

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     kubernetes = ">= 1.13.2"
     azuread    = ">= 1.0.0"
-    azurerm    = ">= 2.33.0"
+    azurerm    = "= 2.33.0"
     random     = ">= 3.0.0"
     null       = ">= 3.0.0"
   }
